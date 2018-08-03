091bbeb2677ec8d8ed5054f7193ad1f6e0a8c046
Javier Romero <romeropaolini@gmail.com>
2018-08-03 17:11:43 +0200

    Connected API

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchArtistService } from '../../services/search-artist.service';
 import { Router } from '@angular/router';
+import { SearchEventsService } from '../../services/search-events.service';
 
 @Component({
   selector: 'app-search-page',
@@ -9,22 +9,21 @@ import { Router } from '@angular/router';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor(private searchArtist: SearchArtistService, private router: Router) { }
+  constructor(private searchEvents: SearchEventsService, private router: Router) { }
   feedbackEnabled = false;
   error = null;
   processing = false;
-  event: string;
+  query: string;
 
   ngOnInit() {
   }
 
   submitForm(searchForm) {
-    console.log(searchForm);
     this.error = '';
     this.feedbackEnabled = true;
     if (searchForm.valid) {
       this.processing = true;
-      this.searchArtist.submit(this.event)
+      this.searchEvents.submit(searchForm.value.search)
         .then((result) => {
           this.router.navigate(['/search/events']);
         })
@@ -36,15 +35,17 @@ export class SearchPageComponent implements OnInit {
 
     }
 
-// ANGULAR
-// primero lograr capturar el input en este TS file.
-// desde el ts file, debes llamar el service(eso significa que debemos crealo y importarlo comp private dentro del contructor)
-// debemos ir al service y crear el metodo que nos comunique ocn node para buscarlos eventos.
+    // ANGULAR
+    // primero lograr capturar el input en este TS file.
+    // desde el ts file, debes llamar el service(eso significa que debemos crealo y importarlo comp private dentro del contructor)
+    // debemos ir al service y crear el metodo que nos comunique ocn node para buscarlos eventos.
 
-// NODE
-// tenemos que ir a node y craear el API endpoint. /events/search
-// dentro de este metodo debemos hacer la query a la api, mandando el nombre del artista y nos va a devolver un archivo json.
+    // NODE
+    // tenemos que ir a node y craear el API endpoint. /events/search
+    // dentro de este metodo debemos hacer la query a la api, mandando el nombre del artista y nos va a devolver un archivo json.
 
-// ANGULAR
-// dentro del ts donde llamamos al service. debemos resolver la promise 
-// (dentro del then), para redireccionar a un paguina donde muestrre los eventos.
+    // ANGULAR
+    // dentro del ts donde llamamos al service. debemos resolver la promise
+    // (dentro del then), para redireccionar a un paguina donde muestrre los eventos
+  }
+}
